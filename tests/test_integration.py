b43faf2f918bf1127f4331db632b744344f8815a
Eric Gazoni <eric@adimian.com>
2020-01-10 14:55:24 +0100

    raise coverage

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,4 @@
-from hlog import Chain
+from hlog import Chain, build_record
 
 
 def test_logging_actions():
@@ -17,4 +17,12 @@ def test_logging_actions():
     assert c2.verify()
 
 
+def test_tampering_raises_an_exception():
+    c = Chain()
 
+    h0 = c.append(message="Alice gives 10.36 euros to Bob", amount=10.36, currency="EUR")
+    h1 = c.append(message="Bob gives 2 dollars to Alice", amount=2, currency="USD")
+    h2 = c.append(message="Bob gives 1 dollar to Charles", amount=1, currency="USD")
+    c.records[1] = build_record({"message": "good"}, previous_hash=c.records[0].hash)
+
+    assert not c.verify(seq=1, hash=h1)
